bdfd687772fd5cee60703cbb3ebbf29fb7f71461
natenhek <nathannhek@csu.fullerton.edu>
2018-11-29 18:08:55 -0800

    Update SetupDB.sql

--- SetupDB.sql
+++ SetupDB.sql
@@ -2,19 +2,25 @@ DROP SCHEMA IF EXISTS bbs;
 CREATE SCHEMA bbs;
 
 create table bbs.bbusers (
+ user_id integer(10) NOT NULL AUTO_INCREMENT,
  email varchar(50),
  name varchar(30),
  password varchar(10),
  nickname varchar(30),
- primary key (email)
+ primary key (user_id)
 ) ENGINE=InnoDB;
+
 create table bbs.postings (
- postId integer(5) auto_increment,
- postDate datetime,
- postedBy varchar(50),
- postSubject varchar(100),
- content varchar(512),
- ancestorPath varchar(100),
- primary key (postId),
- foreign key (postedBy) references bbusers(email) ON UPDATE CASCADE ON DELETE SET NULL
-)ENGINE=InnoDB; 
+ message_id 	integer(10) NOT NULL AUTO_INCREMENT,
+ poster_id 		integer(10) NOT NULL,
+ topic_id 		integer(10) NOT NULL,
+ postedBy 		varchar(50) NOT NULL,
+ subject 		varchar(100),
+ content 		varchar(512) NOT NULL,
+ date 			timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ parent_id		int(10) NOT NULL,	-- references the poster_id / ancestor
+ message_block 	int(10) ,			
+ primary key (message_id),
+ foreign key (poster_id) references bbusers(user_id) ON UPDATE CASCADE 
+) ENGINE=InnoDB; 
+
